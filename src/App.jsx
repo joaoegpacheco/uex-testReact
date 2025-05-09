46b092eb303a9b43f786249308cce63133182a32
João Pacheco <jao.pacheco1@gmail.com>
2025-05-09 10:05:34 -0300

    expect authentication state from context and not from localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import HomePage from './pages/HomePage';
+import { useAuth } from "./contexts/AuthContext";
 
 function App() {
-  const isAuthenticated = !!localStorage.getItem('currentUser');
+  const { currentUser } = useAuth();
+const isAuthenticated = !!currentUser;
 
   return (
     <Routes>
-      <Route path="/" element={!isAuthenticated ? <Navigate to="/login" /> : <Navigate to="/dashboard" />} />
+      <Route path="/" element={!isAuthenticated ? <Navigate to="/login" replace/> : <Navigate to="/dashboard" replace/>} />
       <Route path="/dashboard" element={<HomePage />} />
       <Route path="/login" element={isAuthenticated ? <HomePage /> : <LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
